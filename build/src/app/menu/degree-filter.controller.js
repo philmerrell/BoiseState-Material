f3f4adc7043a61abfbad5b33dfc0d1096ea3a72d
philmerrell <philmerrell@boisestate.edu>
2015-06-25 11:30:06 -0600

    build for gh-pages

--- build/src/app/menu/degree-filter.controller.js
+++ build/src/app/menu/degree-filter.controller.js
@@ -20,6 +20,19 @@
     vm.selectMathRequirement = selectMathRequirement;
     vm.degreeService = DegreeService;
 
+    vm.interests = ["Agriculture", "Arts & Media", "Business & Industry", "Communications", "Computers and Technology",
+      "Design", "Economic and Political Systems", "Education and Human Development", "Engineering", "Health Related",
+      "History", "Interdisciplunary Studies", "Languages and Culture", "Literature", "Management and Entrepreneurship",
+      "Math", "Music", "Natural Resources & Environment", "Performance Art", "Public Service", "Science Related",
+      "Social Behavior & Human Understanding", "Gaming & Computer Graphics", "Social Justice and Diversity",
+      "Construction and Design", "Money and Finance", "Law", "The Human Body"
+    ];
+
+    vm.characteristics = ["Do research", "Innovate", "Lead Others", "Make Decisions", "Multitask", "Organize/Categorize",
+      "Read", "Solve Problems/Puzzles", "Think Creatively", "Think Critically", "Work with Deadlines", "Work with my hands",
+      "Work with Other People", "Write", "Working with Numbers", "Listening", "Taking things apart and putting them back together",
+      "Working with Money/Budgeting", "Working on Computers", "Sports"];
+
     ////////////////////////////
 
     function filterDegreeLength() {
