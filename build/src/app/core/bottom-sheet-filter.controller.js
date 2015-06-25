37c4f65563bfba0a31a43894afc7507e73ee1d3b
philmerrell <philmerrell@boisestate.edu>
2015-06-25 12:06:31 -0600

    add controller files.

---
+++ build/src/app/core/bottom-sheet-filter.controller.js
@@ -0,0 +1,60 @@
+(function() {
+  'use strict';
+
+  angular.module('boisestate')
+    .controller('BottomSheetCtrl', BottomSheetCtrl);
+
+  function BottomSheetCtrl($mdDialog, $mdBottomSheet) {
+
+    var vm = this;
+
+    vm.showInterests = showInterests;
+    vm.showCharacteristics = showCharacteristics;
+    vm.showAdvanced = showAdvanced;
+
+
+
+    function showInterests(ev) {
+      $mdDialog.show({
+        templateUrl: 'core/interests.tpl.html',
+        controller: 'InterestsCtrl as vm',
+        parent: angular.element(document.body),
+        targetEvent: ev
+      })
+        .then(function(answer) {
+          //$scope.alert = 'You said the information was "' + answer + '".';
+        }, function() {
+          //$scope.alert = 'You cancelled the dialog.';
+        });
+    }
+
+    function showCharacteristics(ev) {
+      $mdDialog.show({
+        templateUrl: 'core/characteristics.tpl.html',
+        controller: 'InterestsCtrl as vm',
+        parent: angular.element(document.body),
+        targetEvent: ev
+      })
+        .then(function(answer) {
+          //$scope.alert = 'You said the information was "' + answer + '".';
+        }, function() {
+          //$scope.alert = 'You cancelled the dialog.';
+        });
+    }
+
+    function showAdvanced(ev) {
+      $mdDialog.show({
+        templateUrl: 'core/advanced.tpl.html',
+        controller: 'InterestsCtrl as vm',
+        parent: angular.element(document.body),
+        targetEvent: ev
+      })
+        .then(function(answer) {
+          //$scope.alert = 'You said the information was "' + answer + '".';
+        }, function() {
+          //$scope.alert = 'You cancelled the dialog.';
+        });
+    }
+
+  }
+})();
---
+++ build/src/app/core/interests.controller.js
@@ -0,0 +1,64 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('boisestate')
+    .controller('InterestsCtrl', InterestsCtrl)
+  ;
+
+
+  function InterestsCtrl($mdDialog, $mdBottomSheet, DegreeFilterService) {
+    var vm = this;
+    vm.closeDialog = closeDialog;
+    vm.viewResults = viewResults;
+    vm.selectInterest = selectInterest;
+    vm.interests = ["Agriculture", "Arts & Media", "Business & Industry", "Communications", "Computers and Technology",
+      "Design", "Economic and Political Systems", "Education and Human Development", "Engineering", "Health Related",
+      "History", "Interdisciplunary Studies", "Languages and Culture", "Literature", "Management and Entrepreneurship",
+      "Math", "Music", "Natural Resources & Environment", "Performance Art", "Public Service", "Science Related",
+      "Social Behavior & Human Understanding", "Gaming & Computer Graphics", "Social Justice and Diversity",
+      "Construction and Design", "Money and Finance", "Law", "The Human Body"
+    ];
+
+    vm.characteristics = ["Do research", "Innovate", "Lead Others", "Make Decisions", "Multitask", "Organize/Categorize",
+      "Read", "Solve Problems/Puzzles", "Think Creatively", "Think Critically", "Work with Deadlines", "Work with my hands",
+      "Work with Other People", "Write", "Working with Numbers", "Listening", "Taking things apart and putting them back together",
+      "Working with Money/Budgeting", "Working on Computers", "Sports"];
+
+    function closeDialog() {
+
+      $mdDialog.cancel();
+
+    }
+
+    function viewResults() {
+
+      $mdDialog.hide();
+      $mdBottomSheet.hide();
+    }
+
+    function selectInterest(interest, value, index) {
+
+      var interestObj = {
+        name: interest,
+        type: 'Interest',
+        index: index
+      };
+
+      if (value) {
+
+        vm.interests.push(interestObj);
+        DegreeFilterService.addFilterChip(interestObj);
+
+      } else {
+        var obj = _.find(vm.interests, {'name': interest});
+        _.pull(vm.interests, obj);
+        DegreeFilterService.removeFilter(interestObj);
+      }
+
+    }
+
+
+  }
+
+})();
